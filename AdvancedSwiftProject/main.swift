2c803e0869c895858b5bc6c3142b48c3773e872b
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-25 15:39:37 +0300

    REFERENCE TYPE & VALUE TYPE

--- AdvancedSwiftProject/main.swift
+++ AdvancedSwiftProject/main.swift
@@ -8,6 +8,33 @@
 import Foundation
 
 let classJames = MusicianClass(name: "James", age: 40, instrument: "Guitar")
-print(classJames.age)
+//print(classJames.age)
 
-let structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitar")
+var structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitar")
+//struct kullanırken var yerine let yazarsam değerleri değiştiremem immutable olduğu için.
+//print(structJames.age)
+
+//REFERENCE TYPE & VALUE TYPE
+
+let copyOfClassJames = classJames
+var copyOfStructJames = structJames
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+copyOfClassJames.age = 41
+copyOfStructJames.age = 51
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+print(classJames.age) // it changed
+print(structJames.age) // doesnt change
+
+//Reference Type --> Class
+//copy --> Same object new reference --> 1 object + 2 reference
+
+//Value type --> Struct
+//copy --> new object --> 2 objects
+
+
